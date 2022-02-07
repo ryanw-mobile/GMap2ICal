74121f1855b2c20aa203fb7a5997156c3e069c27
Ryan Wong <ryan@rwmobi.com>
2022-02-07 02:59:13 +0000

    [feature] ActivityType: add more activity types by extracting from real datasets

--- src/main/kotlin/uk/ryanwong/gmap2ics/domain/ActivityType.kt
+++ src/main/kotlin/uk/ryanwong/gmap2ics/domain/ActivityType.kt
@@ -2,14 +2,23 @@ package uk.ryanwong.gmap2ics.domain
 
 enum class ActivityType {
     UNKNOWN_ACTIVITY_TYPE,
-    WALKING,
-    IN_PASSENGER_VEHICLE,
+    BOATING,
+    CYCLING,
+    FLYING,
+    HIKING,
     IN_BUS,
+    IN_CABLECAR,
+    IN_FERRY,
+    IN_PASSENGER_VEHICLE,
+    IN_SUBWAY,
+    IN_TAXI,
     IN_TRAIN,
     IN_TRAM,
-    IN_SUBWAY,
-    IN_FERRY,
-    IN_TAXI,
-    IN_CABLECAR,
-    FLYING
+    IN_VEHICLE,
+    MOTORCYCLING,
+    RUNNING,
+    SAILING,
+    SKIING,
+    STILL,
+    WALKING
 }
