4176f2ad7386896dcf4e5b1722eea2e07c81f752
Ryan Wong <ryan@rwmobi.com>
2022-07-18 01:43:47 +0100

    [test] LatLng: first unit test

--- src/test/kotlin/uk/ryanwong/gmap2ics/domain/models/LatLngTest.kt
+++ src/test/kotlin/uk/ryanwong/gmap2ics/domain/models/LatLngTest.kt
@@ -1,11 +1,20 @@
 package uk.ryanwong.gmap2ics.domain.models
 
-import org.junit.Test
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
 
 class LatLngTest {
 
-    // TODO: pick a test framework
     @Test
-    fun getFormattedLatLng() {
+    fun `should return rounded LatLng in a correct format`() {
+        // given
+        val latitude = 12.12345678
+        val longitude = 123.12345678
+
+        // when
+        val latlng = LatLng(latitude = latitude, longitude = longitude)
+
+        // then
+        latlng.getFormattedLatLng() shouldBe "12.123457,123.123457"
     }
 }
