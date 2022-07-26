95cc23ce9c5fdc7c78784019d26a6da2e48df280
Ryan Wong <ryan@rwmobi.com>
2022-07-26 18:53:37 +0100

    [test] MainScreenViewModelTest: add new unit tests

--- src/test/kotlin/uk/ryanwong/gmap2ics/ui/models/MainScreenViewModelTest.kt
+++ src/test/kotlin/uk/ryanwong/gmap2ics/ui/models/MainScreenViewModelTest.kt
@@ -1,43 +1,286 @@
 package uk.ryanwong.gmap2ics.ui.models
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeTypeOf
+import kotlinx.coroutines.flow.first
 import uk.ryanwong.gmap2ics.configs.MockConfig
 import uk.ryanwong.gmap2ics.data.repository.MockTimelineRepository
+import uk.ryanwong.gmap2ics.domain.models.JFileChooserResult
+import uk.ryanwong.gmap2ics.ui.MainScreenUIState
 import uk.ryanwong.gmap2ics.ui.utils.MockResourceBundle
 
 class MainScreenViewModelTest : FreeSpec() {
 
     lateinit var mainScreenViewModel: MainScreenViewModel
+    private val mockProjectBasePath = "/default-base-path/default-sub-folder/"
 
     private fun setupViewModel() {
         mainScreenViewModel = MainScreenViewModel(
             configFile = MockConfig(),
             timelineRepository = MockTimelineRepository(),
             resourceBundle = MockResourceBundle(),
-            projectBasePath = "/default-base-path/default-sub-folder"
+            projectBasePath = mockProjectBasePath
         )
     }
 
     init {
         "setExportPlaceVisit" - {
+            "should update enablePlacesApiLookup correctly" {
+                // 🔴 Given
+                setupViewModel()
+                val initialState = mainScreenViewModel.exportPlaceVisit.first()
+
+                // 🟡 When
+                mainScreenViewModel.setExportPlaceVisit(enabled = !initialState)
+
+                // 🟢 Then
+                mainScreenViewModel.exportPlaceVisit.first() shouldBe !initialState
+            }
         }
 
         "setExportActivitySegment" - {
+            "should update enablePlacesApiLookup correctly" {
+                // 🔴 Given
+                setupViewModel()
+                val initialState = mainScreenViewModel.exportActivitySegment.first()
+
+                // 🟡 When
+                mainScreenViewModel.setExportActivitySegment(enabled = !initialState)
+
+                // 🟢 Then
+                mainScreenViewModel.exportActivitySegment.first() shouldBe !initialState
+            }
         }
 
         "setEnablePlacesApiLookup" - {
+            "should update enablePlacesApiLookup correctly" {
+                // 🔴 Given
+                setupViewModel()
+                val initialState = mainScreenViewModel.enablePlacesApiLookup.first()
+
+                // 🟡 When
+                mainScreenViewModel.setEnablePlacesApiLookup(enabled = !initialState)
+
+                // 🟢 Then
+                mainScreenViewModel.enablePlacesApiLookup.first() shouldBe !initialState
+            }
         }
 
         "onChangeJsonPath" - {
+            "When mainScreenUIState is Ready" - {
+                "should set mainScreenUIState to ShowChangeJsonPathDialog" {
+                    // 🔴 Given
+                    setupViewModel()
+                    mainScreenViewModel.mainScreenUIState.first() shouldBe MainScreenUIState.Ready
+
+                    // 🟡 When
+                    mainScreenViewModel.onChangeJsonPath()
+
+                    // 🟢 Then
+                    mainScreenViewModel.mainScreenUIState.first() shouldBe MainScreenUIState.ShowChangeJsonPathDialog
+                }
+            }
+
+            "When mainScreenUIState is ShowChangeICalPathDialog" - {
+                "should keep mainScreenUIState unchanged" {
+                    // 🔴 Given
+                    setupViewModel()
+                    mainScreenViewModel.onChangeICalPath()
+
+                    // 🟡 When
+                    mainScreenViewModel.onChangeJsonPath()
+
+                    // 🟢 Then
+                    mainScreenViewModel.mainScreenUIState.first() shouldBe MainScreenUIState.ShowChangeICalPathDialog
+                }
+            }
+
+            "When mainScreenUIState is Error" - {
+                "should keep mainScreenUIState unchanged" {
+                    // 🔴 Given
+                    setupViewModel()
+                    mainScreenViewModel.updateJsonPath(jFileChooserResult = JFileChooserResult.Error(errorCode = 521))
+
+                    // 🟡 When
+                    mainScreenViewModel.onChangeJsonPath()
+
+                    // 🟢 Then
+                    mainScreenViewModel.mainScreenUIState.first().shouldBeTypeOf<MainScreenUIState.Error>()
+                }
+            }
         }
 
         "onChangeICalPath" - {
+            "When mainScreenUIState is Ready" - {
+                "should set mainScreenUIState to ShowChangeJsonPathDialog" {
+                    // 🔴 Given
+                    setupViewModel()
+                    mainScreenViewModel.mainScreenUIState.first() shouldBe MainScreenUIState.Ready
+
+                    // 🟡 When
+                    mainScreenViewModel.onChangeICalPath()
+
+                    // 🟢 Then
+                    mainScreenViewModel.mainScreenUIState.first() shouldBe MainScreenUIState.ShowChangeICalPathDialog
+                }
+            }
+
+            "When mainScreenUIState is ShowChangeICalPathDialog" - {
+                "should keep mainScreenUIState unchanged" {
+                    // 🔴 Given
+                    setupViewModel()
+                    mainScreenViewModel.onChangeJsonPath()
+
+                    // 🟡 When
+                    mainScreenViewModel.onChangeICalPath()
+
+                    // 🟢 Then
+                    mainScreenViewModel.mainScreenUIState.first() shouldBe MainScreenUIState.ShowChangeJsonPathDialog
+                }
+            }
+
+            "When mainScreenUIState is Error" - {
+                "should keep mainScreenUIState unchanged" {
+                    // 🔴 Given
+                    setupViewModel()
+                    mainScreenViewModel.updateJsonPath(jFileChooserResult = JFileChooserResult.Error(errorCode = 521))
+
+                    // 🟡 When
+                    mainScreenViewModel.onChangeICalPath()
+
+                    // 🟢 Then
+                    mainScreenViewModel.mainScreenUIState.first().shouldBeTypeOf<MainScreenUIState.Error>()
+                }
+            }
         }
 
         "updateJsonPath" - {
+            "When JFileChooserResult is AbsolutePath" - {
+                "should correctly trim the path" {
+                    // 🔴 Given
+                    setupViewModel()
+                    mainScreenViewModel.onChangeJsonPath()
+
+                    // 🟡 When
+                    val jFileChooserResult = JFileChooserResult.AbsolutePath(
+                        absolutePath = mockProjectBasePath + "sample-folder1/sample-folder2"
+                    )
+                    mainScreenViewModel.updateJsonPath(jFileChooserResult = jFileChooserResult)
+
+                    // 🟢 Then
+                    mainScreenViewModel.jsonPath.first() shouldBe "sample-folder1/sample-folder2"
+                }
+
+                "should set MainScreenUIState = Ready" {
+                    // 🔴 Given
+                    setupViewModel()
+                    mainScreenViewModel.onChangeJsonPath()
+
+                    // 🟡 When
+                    val jFileChooserResult = JFileChooserResult.AbsolutePath(
+                        absolutePath = mockProjectBasePath + "sample-folder1/sample-folder2"
+                    )
+                    mainScreenViewModel.updateJsonPath(jFileChooserResult = jFileChooserResult)
+
+                    // 🟢 Then
+                    mainScreenViewModel.mainScreenUIState.first() shouldBe MainScreenUIState.Ready
+                }
+            }
+
+            "When JFileChooserResult is Cancelled" - {
+                "should set MainScreenUIState = Ready" {
+                    // 🔴 Given
+                    setupViewModel()
+                    mainScreenViewModel.onChangeJsonPath()
+
+                    // 🟡 When
+                    mainScreenViewModel.updateJsonPath(jFileChooserResult = JFileChooserResult.Cancelled)
+
+                    // 🟢 Then
+                    mainScreenViewModel.mainScreenUIState.first() shouldBe MainScreenUIState.Ready
+                }
+            }
+
+
+            "When JFileChooserResult is Error" - {
+                "should set MainScreenUIState = Error with correct error message" {
+                    // 🔴 Given
+                    setupViewModel()
+                    mainScreenViewModel.onChangeJsonPath()
+
+                    // 🟡 When
+                    mainScreenViewModel.updateJsonPath(jFileChooserResult = JFileChooserResult.Error(errorCode = 521))
+
+                    // 🟢 Then
+                    val mainScreenUIState = mainScreenViewModel.mainScreenUIState.first()
+                    (mainScreenUIState as MainScreenUIState.Error).errMsg shouldBe "mock-string-for-error.updating.json.path"
+                }
+            }
         }
 
         "updateICalPath" - {
+            "When JFileChooserResult is AbsolutePath" - {
+                "should correctly trim the path" {
+                    // 🔴 Given
+                    setupViewModel()
+                    mainScreenViewModel.onChangeICalPath()
+
+                    // 🟡 When
+                    val jFileChooserResult = JFileChooserResult.AbsolutePath(
+                        absolutePath = mockProjectBasePath + "sample-folder1/sample-folder2"
+                    )
+                    mainScreenViewModel.updateICalPath(jFileChooserResult = jFileChooserResult)
+
+                    // 🟢 Then
+                    mainScreenViewModel.iCalPath.first() shouldBe "sample-folder1/sample-folder2"
+                }
+
+                "should set MainScreenUIState = Ready" {
+                    // 🔴 Given
+                    setupViewModel()
+                    mainScreenViewModel.onChangeICalPath()
+
+                    // 🟡 When
+                    val jFileChooserResult = JFileChooserResult.AbsolutePath(
+                        absolutePath = mockProjectBasePath + "sample-folder1/sample-folder2"
+                    )
+                    mainScreenViewModel.updateICalPath(jFileChooserResult = jFileChooserResult)
+
+                    // 🟢 Then
+                    mainScreenViewModel.mainScreenUIState.first() shouldBe MainScreenUIState.Ready
+                }
+            }
+
+            "When JFileChooserResult is Cancelled" - {
+                "should set MainScreenUIState = Ready" {
+                    // 🔴 Given
+                    setupViewModel()
+                    mainScreenViewModel.onChangeICalPath()
+
+                    // 🟡 When
+                    mainScreenViewModel.updateICalPath(jFileChooserResult = JFileChooserResult.Cancelled)
+
+                    // 🟢 Then
+                    mainScreenViewModel.mainScreenUIState.first() shouldBe MainScreenUIState.Ready
+                }
+            }
+
+
+            "When JFileChooserResult is Error" - {
+                "should set MainScreenUIState = Error with correct error message" {
+                    // 🔴 Given
+                    setupViewModel()
+                    mainScreenViewModel.onChangeICalPath()
+
+                    // 🟡 When
+                    mainScreenViewModel.updateICalPath(jFileChooserResult = JFileChooserResult.Error(errorCode = 521))
+
+                    // 🟢 Then
+                    val mainScreenUIState = mainScreenViewModel.mainScreenUIState.first()
+                    (mainScreenUIState as MainScreenUIState.Error).errMsg shouldBe "mock-string-for-error.updating.ical.path"
+                }
+            }
         }
     }
 }
