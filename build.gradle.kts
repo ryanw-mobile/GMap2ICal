fa8d147b02e6f97ec2fa47290cc3cb66efc21835
Ryan Wong <ryan@rwmobi.com>
2022-07-30 05:01:04 +0100

    [refactor] App: reload project Compose Desktop template

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,52 +1,68 @@
 import org.jetbrains.compose.compose
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+}
 
 group = "uk.ryanwong"
 version = "1.0-SNAPSHOT"
 
-plugins {
-    kotlin("jvm") version "1.7.0"
-    id("org.jetbrains.compose") version "1.2.0-alpha01-dev750"
-    id("io.kotest.multiplatform") version "5.0.2"
-}
-
 repositories {
+    google()
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    google()
 }
 
 val ktorVersion = "2.0.3"
-dependencies {
-    implementation(compose.desktop.currentOs)
-    testImplementation(kotlin("test"))
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.3")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
-    implementation("us.dustinj.timezonemap:timezonemap:4.5")
-    implementation("com.squareup.moshi:moshi:1.13.0")
-    implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.2")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
-    implementation("io.ktor:ktor-client-core:$ktorVersion")
-    implementation("io.ktor:ktor-client-cio:$ktorVersion")
-    // kotest
-    testImplementation("io.kotest:kotest-runner-junit5:5.3.1")
-    testImplementation("io.kotest:kotest-assertions-core:5.3.1")
-    testImplementation("io.kotest:kotest-property:5.3.1")
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "11"
+        }
+        withJava()
+    }
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(compose.desktop.currentOs)
+                implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.3")
+                implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
+                implementation("us.dustinj.timezonemap:timezonemap:4.5")
+                implementation("com.squareup.moshi:moshi:1.13.0")
+                implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
+                implementation("com.squareup.retrofit2:retrofit:2.9.0")
+                implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.2")
+                implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-cio:$ktorVersion")
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                // kotest
+                implementation("io.kotest:kotest-runner-junit5:5.3.1")
+                implementation("io.kotest:kotest-assertions-core:5.3.1")
+                implementation("io.kotest:kotest-property:5.3.1")
+            }
+        }
+    }
 }
 
 compose.desktop {
     application {
         mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "GMapTimelineToICS"
+            packageVersion = "1.0.0"
+        }
     }
 }
 
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,4 @@
-rootProject.name = "GMapTimeLineToICS"
-
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 pluginManagement {
     repositories {
         google()
@@ -7,4 +6,12 @@ pluginManagement {
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
+
+    plugins {
+        kotlin("multiplatform").version(extra["kotlin.version"] as String)
+        id("org.jetbrains.compose").version(extra["compose.version"] as String)
+    }
 }
+
+rootProject.name = "GMapTimelineToICS"
+
