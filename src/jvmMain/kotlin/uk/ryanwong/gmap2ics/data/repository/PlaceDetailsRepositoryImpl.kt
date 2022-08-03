c2d59a0c4cbb5106e0e27bd352a3c59c282c2c26
Ryan Wong <ryan@rwmobi.com>
2022-08-03 21:30:52 +0100

    [refactor] PlaceDetailsRepositoryImpl: simplify code

--- src/jvmMain/kotlin/uk/ryanwong/gmap2ics/data/repository/PlaceDetailsRepositoryImpl.kt
+++ src/jvmMain/kotlin/uk/ryanwong/gmap2ics/data/repository/PlaceDetailsRepositoryImpl.kt
@@ -4,6 +4,9 @@
 
 package uk.ryanwong.gmap2ics.data.repository
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import uk.ryanwong.gmap2ics.app.models.PlaceDetails
 import uk.ryanwong.gmap2ics.configs.Config
 import uk.ryanwong.gmap2ics.data.except
@@ -13,40 +16,43 @@ import kotlin.coroutines.cancellation.CancellationException
 
 class PlaceDetailsRepositoryImpl(
     private val configFile: Config,
-    private val networkDataSource: GoogleApiDataSource = RetrofitGoogleApiDataSource()
+    private val networkDataSource: GoogleApiDataSource = RetrofitGoogleApiDataSource(),
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : PlaceDetailsRepository {
 
     private val placesCache = mutableMapOf<String, PlaceDetails>()
 
     override suspend fun getPlaceDetails(placeId: String, placeTimeZoneId: String?): Result<PlaceDetails> {
-        if (placesCache.contains(key = placeId)) {
-            return Result.success(placesCache.getValue(placeId))
+        return withContext(dispatcher) {
+            if (placesCache.contains(key = placeId)) {
+                Result.success(placesCache.getValue(placeId))
+            }
+
+            // Do API lookup and cache the results
+            // If user does not supply an API Key means we always return null
+            configFile.placesApiKey?.let { apiKey ->
+                val language: String? = configFile.apiLanguageOverride.getOrDefault(
+                    key = placeTimeZoneId,
+                    defaultValue = configFile.apiLanguageOverride.get(key = "default")
+                )
+
+                Result.runCatching {
+                    val placeDetailsDataObject = language?.let {
+                        networkDataSource.getPlaceDetails(placeId = placeId, key = apiKey, language = it)
+                    } ?: networkDataSource.getPlaceDetails(placeId = placeId, key = apiKey)
+
+                    if (!placeDetailsDataObject.isSuccessful) {
+                        throw Exception("⛔️ Error getting API results: ${placeDetailsDataObject.message()}")
+                    }
+
+                    placeDetailsDataObject.body()?.result?.let { result ->
+                        PlaceDetails.from(placeDetailsResult = result)
+                            .also { placeDetailsDomainObject ->
+                                placesCache[placeId] = placeDetailsDomainObject
+                            }
+                    } ?: throw Exception("not found")
+                }.except<CancellationException, _>()
+            } ?: Result.failure(Exception("not found"))
         }
-
-        // Do API lookup and cache the results
-        // If user does not supply an API Key means we always return null
-        return configFile.placesApiKey?.let { apiKey ->
-            val language: String? = configFile.apiLanguageOverride.getOrDefault(
-                key = placeTimeZoneId,
-                defaultValue = configFile.apiLanguageOverride.get(key = "default")
-            )
-
-            Result.runCatching {
-                val placeDetailsDataObject = language?.let {
-                    networkDataSource.getPlaceDetails(placeId = placeId, key = apiKey, language = it)
-                } ?: networkDataSource.getPlaceDetails(placeId = placeId, key = apiKey)
-
-                if (!placeDetailsDataObject.isSuccessful) {
-                    throw Exception("⛔️ Error getting API results: ${placeDetailsDataObject.message()}")
-                }
-
-                placeDetailsDataObject.body()?.result?.let { result ->
-                    PlaceDetails.from(placeDetailsResult = result)
-                        .also { placeDetailsDomainObject ->
-                            placesCache[placeId] = placeDetailsDomainObject
-                        }
-                } ?: throw Exception("not found")
-            }.except<CancellationException, _>()
-        } ?: Result.failure(Exception("not found"))
     }
 }
