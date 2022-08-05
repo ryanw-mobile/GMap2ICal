9f5238c540df3dcc7434c04968c70009b9544414
Ryan Wong <ryan@rwmobi.com>
2022-08-05 23:04:04 +0100

    [test] use cases: formalise setupUseCase()
    👨🏻‍🦲 When I miss Greg, I write tests

--- src/jvmTest/kotlin/uk/ryanwong/gmap2ics/ui/usecases/ExportActivitySegmentUseCaseImplTest.kt
+++ src/jvmTest/kotlin/uk/ryanwong/gmap2ics/ui/usecases/ExportActivitySegmentUseCaseImplTest.kt
@@ -5,14 +5,23 @@
 package uk.ryanwong.gmap2ics.ui.usecases
 
 import io.kotest.core.spec.style.FreeSpec
+import uk.ryanwong.gmap2ics.data.repository.MockPlaceDetailsRepository
+import us.dustinj.timezonemap.TimeZoneMap
 
 internal class ExportActivitySegmentUseCaseImplTest : FreeSpec() {
 
     lateinit var exportActivitySegmentUseCaseImpl: ExportActivitySegmentUseCaseImpl
+    lateinit var mockPlaceDetailsRepository: MockPlaceDetailsRepository
 
     private fun setupUseCase() {
+        mockPlaceDetailsRepository = MockPlaceDetailsRepository()
 
+        exportActivitySegmentUseCaseImpl = ExportActivitySegmentUseCaseImpl(
+            placeDetailsRepository = mockPlaceDetailsRepository,
+            timeZoneMap = TimeZoneMap.forEverywhere() // TODO: MockK required
+        )
     }
+
 
     init {
 
--- src/jvmTest/kotlin/uk/ryanwong/gmap2ics/ui/usecases/ExportChildVisitUseCaseImplTest.kt
+++ src/jvmTest/kotlin/uk/ryanwong/gmap2ics/ui/usecases/ExportChildVisitUseCaseImplTest.kt
@@ -5,13 +5,21 @@
 package uk.ryanwong.gmap2ics.ui.usecases
 
 import io.kotest.core.spec.style.FreeSpec
+import uk.ryanwong.gmap2ics.data.repository.MockPlaceDetailsRepository
+import us.dustinj.timezonemap.TimeZoneMap
 
 internal class ExportChildVisitUseCaseImplTest : FreeSpec() {
 
-    lateinit var exportChildVisitUseCaseImpl: ExportChildVisitUseCaseImpl
+    lateinit var exportChildVisitUseCase: ExportChildVisitUseCaseImpl
+    lateinit var mockPlaceDetailsRepository: MockPlaceDetailsRepository
 
     private fun setupUseCase() {
+        mockPlaceDetailsRepository = MockPlaceDetailsRepository()
 
+        exportChildVisitUseCase = ExportChildVisitUseCaseImpl(
+            placeDetailsRepository = mockPlaceDetailsRepository,
+            timeZoneMap = TimeZoneMap.forEverywhere() // TODO: MockK required
+        )
     }
 
     init {
