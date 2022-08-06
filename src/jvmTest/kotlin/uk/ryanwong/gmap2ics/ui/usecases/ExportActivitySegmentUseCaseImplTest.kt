a2e247b776a112b58e09bc35fffe5479fe2b6d87
Ryan Wong <ryan@rwmobi.com>
2022-08-06 12:24:34 +0100

    [test] unit tests: add test plans
    👨🏻‍🦲 When I miss Greg, I write tests

--- src/jvmTest/kotlin/uk/ryanwong/gmap2ics/ui/usecases/ExportActivitySegmentUseCaseImplTest.kt
+++ src/jvmTest/kotlin/uk/ryanwong/gmap2ics/ui/usecases/ExportActivitySegmentUseCaseImplTest.kt
@@ -4,21 +4,39 @@
 
 package uk.ryanwong.gmap2ics.ui.usecases
 
+import com.esri.core.geometry.Polygon
 import io.kotest.core.spec.style.FreeSpec
+import io.mockk.every
+import io.mockk.mockkObject
 import uk.ryanwong.gmap2ics.data.repository.MockPlaceDetailsRepository
+import us.dustinj.timezonemap.TimeZone
 import us.dustinj.timezonemap.TimeZoneMap
 
 internal class ExportActivitySegmentUseCaseImplTest : FreeSpec() {
+
+    /**
+     * Test Plan
+     * 1. If the given activityType is in ignoredActivityType: always throw IgnoredActivityTypeException
+     * 2. If the given activityType cannot be resolved, will be assigned  ActivityType.UNKNOWN_ACTIVITY_TYPE and export normally
+     * 3. Otherwise should behave normally.
+     * TODO: activitySegment.asTimelineItem requires repository. Possibly move that to this usecase and test it.
+     */
 
     lateinit var exportActivitySegmentUseCaseImpl: ExportActivitySegmentUseCaseImpl
     lateinit var mockPlaceDetailsRepository: MockPlaceDetailsRepository
 
     private fun setupUseCase() {
+        val timeZoneMap = TimeZoneMap.forEverywhere()
+        mockkObject(timeZoneMap)
+        every { timeZoneMap.getOverlappingTimeZone(any(), any()) } returns TimeZone(
+            zoneId = "Asia/Tokyo", // Needs real zone as it affects time calculation
+            region = Polygon()
+        )
         mockPlaceDetailsRepository = MockPlaceDetailsRepository()
 
         exportActivitySegmentUseCaseImpl = ExportActivitySegmentUseCaseImpl(
             placeDetailsRepository = mockPlaceDetailsRepository,
-            timeZoneMap = TimeZoneMap.forEverywhere() // TODO: MockK required
+            timeZoneMap = timeZoneMap
         )
     }
 
--- src/jvmTest/kotlin/uk/ryanwong/gmap2ics/ui/usecases/ExportChildVisitUseCaseImplTest.kt
+++ src/jvmTest/kotlin/uk/ryanwong/gmap2ics/ui/usecases/ExportChildVisitUseCaseImplTest.kt
@@ -4,21 +4,38 @@
 
 package uk.ryanwong.gmap2ics.ui.usecases
 
+import com.esri.core.geometry.Polygon
 import io.kotest.core.spec.style.FreeSpec
+import io.mockk.every
+import io.mockk.mockkObject
 import uk.ryanwong.gmap2ics.data.repository.MockPlaceDetailsRepository
+import us.dustinj.timezonemap.TimeZone
 import us.dustinj.timezonemap.TimeZoneMap
 
 internal class ExportChildVisitUseCaseImplTest : FreeSpec() {
+
+    /**
+     * Test Plan - Very much like the ExportPlaceVisitUseCase, but Child Visit contains more optional data fields
+     * 1. If the given placeId is in ignoredVisitedPlaceIds: always return null for all enablePlacesApiLookup cases
+     * 2. If !enablePlacesApiLookup, just convert to timeline and then VEvent
+     * 3. If enablePlacesApiLookup, either add getPlaceDetails() to timeline then VEvent, else same as #2
+     */
 
     lateinit var exportChildVisitUseCase: ExportChildVisitUseCaseImpl
     lateinit var mockPlaceDetailsRepository: MockPlaceDetailsRepository
 
     private fun setupUseCase() {
+        val timeZoneMap = TimeZoneMap.forEverywhere()
+        mockkObject(timeZoneMap)
+        every { timeZoneMap.getOverlappingTimeZone(any(), any()) } returns TimeZone(
+            zoneId = "Asia/Tokyo", // Needs real zone as it affects time calculation
+            region = Polygon()
+        )
         mockPlaceDetailsRepository = MockPlaceDetailsRepository()
 
         exportChildVisitUseCase = ExportChildVisitUseCaseImpl(
             placeDetailsRepository = mockPlaceDetailsRepository,
-            timeZoneMap = TimeZoneMap.forEverywhere() // TODO: MockK required
+            timeZoneMap = timeZoneMap
         )
     }
 
