9bcf462f6208fc78c3e4c42d44d5b9eb01ebc56f
Ryan Wong <ryan@rwmobi.com>
2022-08-15 00:01:32 +0100

    [chore] PlaceDetailApi: add back required ExperimentalSerializationApi annotation

--- src/jvmMain/kotlin/uk/ryanwong/gmap2ics/data/source/googleapi/retrofit/PlaceDetailApi.kt
+++ src/jvmMain/kotlin/uk/ryanwong/gmap2ics/data/source/googleapi/retrofit/PlaceDetailApi.kt
@@ -6,10 +6,12 @@ package uk.ryanwong.gmap2ics.data.source.googleapi.retrofit
 
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType
 import retrofit2.Retrofit
 
+@OptIn(ExperimentalSerializationApi::class)
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(Json.asConverterFactory(MediaType.get("application/json")))
     .addCallAdapterFactory(CoroutineCallAdapterFactory())
