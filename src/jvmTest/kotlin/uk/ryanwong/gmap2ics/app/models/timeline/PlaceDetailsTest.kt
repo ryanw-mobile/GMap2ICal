a796ad69eb6dd2db691c6e04777ee5f379561925
Ryan Wong <ryan@rwmobi.com>
2022-08-15 13:26:25 +0100

    [test] PlaceDetailsTest: implement unit tests (100% coverage)
    👨🏻‍🦲

---
+++ src/jvmTest/kotlin/uk/ryanwong/gmap2ics/app/models/timeline/PlaceDetailsTest.kt
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2022. Ryan Wong (hello@ryanwong.co.uk)
+ */
+
+package uk.ryanwong.gmap2ics.app.models.timeline
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import uk.ryanwong.gmap2ics.app.models.timeline.PlaceDetailsTestData.mockPlaceDetailsResult
+
+internal class PlaceDetailsTest : FreeSpec() {
+
+    init {
+        "should convert correctly from data.models.places.Result to PLaceDetails" {
+            // 🔴 Given
+            val placeDetailsResult = mockPlaceDetailsResult
+
+            // 🟡 When
+            val placeDetails = PlaceDetails.from(placeDetailsResult = placeDetailsResult)
+
+            // 🟢 Then
+            placeDetails shouldBe PlaceDetails(
+                placeId = "ChIJOX4_ortjeUgR2_LUcFpvLg4",
+                name = "Yorkshire Sculpture Park",
+                formattedAddress = "Wakefield WF4 4LG, UK",
+                geo = LatLng(latitude = 53.6152405, longitude = -1.5639315),
+                types = listOf(
+                    "art_gallery",
+                    "tourist_attraction",
+                    "park",
+                    "museum",
+                    "point_of_interest",
+                    "establishment"
+                ),
+                url = "https://maps.google.com/?cid=1021876599690425051"
+            )
+        }
+
+        "getFormattedName" - {
+            "should format the name with a correct emoji if PlaceType is known" {
+                // 🔴 Given
+                val placeDetails = PlaceDetails(
+                    placeId = "some-place-id",
+                    name = "some-name",
+                    formattedAddress = "some-formatted-address",
+                    geo = LatLng(latitude = 53.6152405, longitude = -1.5639315),
+                    types = listOf(
+                        "art_gallery",
+                        "tourist_attraction",
+                        "park",
+                        "museum",
+                        "point_of_interest",
+                        "establishment"
+                    ),
+                    url = "https://maps.google.com/?cid=1021876599690425051"
+                )
+
+                // 🟡 When
+                val formattedName = placeDetails.getFormattedName()
+
+                // 🟢 Then
+                formattedName shouldBe "\uD83D\uDDBC some-name"
+            }
+
+            "should format the name with a default emoji for unknown PlaceType" {
+                // 🔴 Given
+                val placeDetails = PlaceDetails(
+                    placeId = "some-place-id",
+                    name = "some-name",
+                    formattedAddress = "some-formatted-address",
+                    geo = LatLng(latitude = 53.6152405, longitude = -1.5639315),
+                    types = listOf(
+                        "some-unknown-type"
+                    ),
+                    url = "https://maps.google.com/?cid=1021876599690425051"
+                )
+
+                // 🟡 When
+                val formattedName = placeDetails.getFormattedName()
+
+                // 🟢 Then
+                formattedName shouldBe "\uD83D\uDCCD some-name"
+            }
+        }
+    }
+}
---
+++ src/jvmTest/kotlin/uk/ryanwong/gmap2ics/app/models/timeline/PlaceDetailsTestData.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2022. Ryan Wong (hello@ryanwong.co.uk)
+ */
+
+package uk.ryanwong.gmap2ics.app.models.timeline
+
+import uk.ryanwong.gmap2ics.data.models.places.AddressComponent
+import uk.ryanwong.gmap2ics.data.models.places.Geometry
+import uk.ryanwong.gmap2ics.data.models.places.Location
+import uk.ryanwong.gmap2ics.data.models.places.PlusCode
+import uk.ryanwong.gmap2ics.data.models.places.Result
+
+object PlaceDetailsTestData {
+    /***
+     * Note: Seeing real data would help understand what kind of data we are getting from Google.
+     */
+
+    val mockPlaceDetailsResult = Result(
+        addressComponents = listOf(
+            AddressComponent(longName = "Wakefield", shortName = "Wakefield", types = listOf("postal_town")),
+            AddressComponent(
+                longName = "West Yorkshire",
+                shortName = "West Yorkshire",
+                types = listOf("administrative_area_level_2", "political")
+            ),
+            AddressComponent(
+                longName = "England",
+                shortName = "England",
+                types = listOf("administrative_area_level_1", "political")
+            ),
+            AddressComponent(longName = "United Kingdom", shortName = "GB", types = listOf("country", "political")),
+            AddressComponent(longName = "WF4 4LG", shortName = "WF4 4LG", types = listOf("postal_code"))
+        ),
+        adrAddress = "<span class=\"locality\">Wakefield</span> <span class=\"postal-code\">WF4 4LG</span>, <span class=\"country-name\">UK</span>",
+        formattedAddress = "Wakefield WF4 4LG, UK",
+        formattedPhoneNumber = "01924 832631",
+        geometry = Geometry(
+            location = Location(
+                lat = 53.6152405,
+                lng = -1.5639315
+            )
+        ),
+        icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/generic_business-71.png",
+        iconBackgroundColor = "#7B9EB0",
+        iconMaskBaseUri = "https://maps.gstatic.com/mapfiles/place_api/icons/v2/generic_pinlet",
+        name = "Yorkshire Sculpture Park",
+        placeId = "ChIJOX4_ortjeUgR2_LUcFpvLg4",
+        plusCode = PlusCode(compoundCode = "JC8P+3C Wakefield, UK, globalCode=9C5WJC8P+3C"),
+        reference = "ChIJOX4_ortjeUgR2_LUcFpvLg4",
+        types = listOf("art_gallery", "tourist_attraction", "park", "museum", "point_of_interest", "establishment"),
+        url = "https://maps.google.com/?cid=1021876599690425051",
+        userRatingsTotal = 8319,
+        utcOffset = 60,
+        vicinity = "Wakefield",
+        website = "http://www.ysp.org.uk/"
+    )
+}
