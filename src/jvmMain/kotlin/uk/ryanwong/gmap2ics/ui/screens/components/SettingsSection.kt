1f30c4f40aee239f0a55312da7c02d8ab423bae0
Ryan Wong <ryan@rwmobi.com>
2022-08-20 02:49:57 +0100

    [feature] Export options: draft composable

--- src/jvmMain/kotlin/uk/ryanwong/gmap2ics/ui/screens/components/SettingsSection.kt
+++ src/jvmMain/kotlin/uk/ryanwong/gmap2ics/ui/screens/components/SettingsSection.kt
@@ -4,14 +4,23 @@
 
 package uk.ryanwong.gmap2ics.ui.screens.components
 
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import java.util.ResourceBundle
 
@@ -85,6 +94,41 @@ fun SettingsSection(
                     onCheckedChange = onVerboseLogsChanged
                 )
             }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ExportOptionsGroup() {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .border(width = 1.dp, color = Color.LightGray)
+            .wrapContentSize()
+    ) {
+        Text(
+            text = "Export Options",
+            textAlign = TextAlign.Center,
+            color = Color.White,
+            modifier = Modifier.fillMaxWidth()
+                .wrapContentHeight()
+                .padding(8.dp)
+                .background(color = Color.DarkGray)
+                .padding(horizontal = 8.dp, vertical = 4.dp)
+        )
+        Row() {
+            CheckBoxItem(
+                text = "123",
+                checked = true,
+                onCheckedChange = { }
+            )
+
+            CheckBoxItem(
+                text = "123",
+                checked = true,
+                onCheckedChange = { }
+            )
         }
     }
 }
