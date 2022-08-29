895e50b3f16c0eaff1c15a716b760119595c0d48
Ryan Wong <ryan@rwmobi.com>
2022-08-29 19:22:08 +0100

    [test] LocalDataSourceImplTest: add more unit tests
    👨🏻‍🦲

--- src/jvmTest/kotlin/uk/ryanwong/gmap2ics/data/source/local/impl/LocalDataSourceImplTest.kt
+++ src/jvmTest/kotlin/uk/ryanwong/gmap2ics/data/source/local/impl/LocalDataSourceImplTest.kt
@@ -4,12 +4,14 @@
 
 package uk.ryanwong.gmap2ics.data.source.local.impl
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.core.test.TestCase
 import io.kotest.engine.spec.tempdir
 import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldEndWith
 import io.kotest.matchers.types.beInstanceOf
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
@@ -17,6 +19,8 @@ import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
 import uk.ryanwong.gmap2ics.data.source.local.LocalDataSource
 import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileWriter
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class LocalDataSourceImplTest : FreeSpec() {
@@ -38,6 +42,10 @@ class LocalDataSourceImplTest : FreeSpec() {
             File(tempDir.absolutePath + "/some-temp-file-3.json").apply { createNewFile() },
             File(tempDir.absolutePath + "/some-temp-file-4.ics").apply { createNewFile() },
         )
+
+        FileWriter(tempDir.absolutePath + "/some-temp-file-5", false).use { fileWriter ->
+            fileWriter.write("some-file-contents")
+        }
     }
 
     private fun setupLocalDataSource() {
@@ -74,7 +82,7 @@ class LocalDataSourceImplTest : FreeSpec() {
                 }
             }
 
-            "Should return empty list if no file matching the extension" {
+            "Should return empty list if no files matching the extension" {
                 setupLocalDataSource()
                 scope.runTest {
                     // 🔴 Given
@@ -113,10 +121,82 @@ class LocalDataSourceImplTest : FreeSpec() {
             }
         }
 
-        "readStringFromFile" {
+        "readStringFromFile" - {
+            "Should return correct file contents if the file exists" {
+                setupLocalDataSource()
+                scope.runTest {
+                    // 🔴 Given
+                    val absolutePath = tempDir.absolutePath + "/some-temp-file-5"
+
+                    // 🟡 When
+                    val fileContents = localDataSource.readStringFromFile(
+                        filePath = absolutePath,
+                    )
+
+                    // 🟢 Then
+                    fileContents shouldBe "some-file-contents"
+                }
+            }
+
+            "Should throw FileNotFoundException if the file does not exist" {
+                setupLocalDataSource()
+                scope.runTest {
+                    // 🔴 Given
+                    val absolutePath = tempDir.absolutePath + "/some-invalid-file-path"
+
+
+                    // 🟡 When
+                    val exception = shouldThrow<FileNotFoundException> {
+                        localDataSource.readStringFromFile(
+                            filePath = absolutePath,
+                        )
+                    }
+
+                    // 🟢 Then
+                    exception.message shouldEndWith "(No such file or directory)"
+                }
+            }
         }
 
-        "fileWriter" {
+        "fileWriter" - {
+            "Should correctly write the contents to the specified filepath" {
+                setupLocalDataSource()
+                scope.runTest {
+                    // 🔴 Given
+                    val absolutePath = tempDir.absolutePath + "/some-file-writer-path"
+                    val contents = "some-contents\\nsome-more-contents"
+
+                    // 🟡 When
+                    val result = localDataSource.fileWriter(
+                        filePath = absolutePath,
+                        contents = contents,
+                    )
+
+                    // 🟢 Then
+                    result.isSuccess shouldBe true
+                    val fileContents = localDataSource.readStringFromFile(absolutePath)
+                    fileContents shouldBe contents
+                }
+            }
+
+            "Should return Failure if the given file path is not writable" {
+                setupLocalDataSource()
+                scope.runTest {
+                    // 🔴 Given
+                    val absolutePath = tempDir.absolutePath
+
+                    // 🟡 When
+                    val result = localDataSource.fileWriter(
+                        filePath = absolutePath,
+                        contents = "some-contents",
+                    )
+
+                    // 🟢 Then
+                    result.isFailure shouldBe true
+                    result.exceptionOrNull() should beInstanceOf<FileNotFoundException>()
+                    result.exceptionOrNull()!!.message shouldEndWith "(Is a directory)"
+                }
+            }
         }
     }
 }
