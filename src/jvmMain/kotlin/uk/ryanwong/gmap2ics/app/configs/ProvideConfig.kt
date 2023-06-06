fc6b3681be74c1eccab09125d0e7604e529c9a39
Ryan Wong <ryan@rwmobi.com>
2023-06-06 01:30:10 +0100

    [chore] ProvideConfig: add comment to prompt changing config file

--- src/jvmMain/kotlin/uk/ryanwong/gmap2ics/app/configs/ProvideConfig.kt
+++ src/jvmMain/kotlin/uk/ryanwong/gmap2ics/app/configs/ProvideConfig.kt
@@ -6,6 +6,7 @@ package uk.ryanwong.gmap2ics.app.configs
 
 class ProvideConfig {
     companion object {
+        // TODO: You should see this as an error, change it to DefaultConfig() and fill in your arguments
         fun getConfig() = RyanConfig()
     }
 }
