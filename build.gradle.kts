50686f8fc6e95a613f9a90899a04a928e2ab159f
Ryan Wong <ryan@rwmobi.com>
2023-12-16 17:57:42 +0800

    [fix] App: fix org.jetbrains.skiko.LibraryLoadException: Cannot find libskiko-macos-arm64.dylib.sha256, proper native dependency missing

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,34 @@ kotlin {
         withJava()
     }
 
-    @Suppress("UNUSED_VARIABLE")
+    // https://github.com/JetBrains/compose-multiplatform/issues/3123
+    val osName = System.getProperty("os.name")
+    val targetOs = when {
+        osName == "Mac OS X" -> "macos"
+        osName.startsWith("Win") -> "windows"
+        osName.startsWith("Linux") -> "linux"
+        else -> error("Unsupported OS: $osName")
+    }
+
+    val targetArch = when (val osArch = System.getProperty("os.arch")) {
+        "x86_64", "amd64" -> "x64"
+        "aarch64" -> "arm64"
+        else -> error("Unsupported arch: $osArch")
+    }
+
+    val version = "0.7.70" // or any more recent version
+    val target = "${targetOs}-${targetArch}"
+
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation("org.jetbrains.skiko:skiko-awt-runtime-$target:$version")
+                implementation(compose.desktop.macos_arm64)
+                // implementation(compose.desktop.currentOs)
+            }
+        }
+    }
+
     sourceSets {
         val commonMain by getting {
             dependencies {
