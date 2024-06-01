d56781be2332eec2c197a3f50dc7e774224978e9
Ryan Wong <ryan@rwmobi.com>
2024-06-01 21:50:06 +0100

    [chore] Gradle: Kotlin/Compose Compiler 2.0 migration

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
-    alias(libs.plugins.compose)apply false
+    alias(libs.plugins.compose) apply false
     alias(libs.plugins.multiplatform) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
---
+++ composeApp/src/desktopTest/kotlin/uk/ryanwong/gmap2ics/ui/screens/components/StatusBarKtTest.kt
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2022-2024. Ryan Wong (hello@ryanwebmail.com)
+ */
+
+package uk.ryanwong.gmap2ics.ui.screens.components
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import gmap2ical.composeapp.generated.resources.Res
+import gmap2ical.composeapp.generated.resources.content_description_linear_progress_indicator
+import kotlinx.coroutines.test.runTest
+import org.jetbrains.compose.resources.getString
+import org.junit.Rule
+import uk.ryanwong.gmap2ics.ui.theme.gregorygreen.GregoryGreenTheme
+import kotlin.test.Test
+
+internal class StatusBarKtTest {
+
+    /***
+     * 1. Currently, Compose Desktop test runs on JUnit4 only.
+     * 2. Can't sort out how to apply @get:Rule on Kotest so back to JUnit4.
+     * 3. Compose Desktop assertion library is not completed yet - limited what can be tested.
+     */
+    @get:Rule
+    val compose = createComposeRule()
+
+    @Test
+    fun `Should not display LinearProgressIndicator if progress is null`() = runTest {
+        with(compose) {
+            val statusMessage = "some-status-message"
+            val progress = null
+
+            setContent {
+                GregoryGreenTheme {
+                    StatusBar(
+                        statusMessage = statusMessage,
+                        progress = progress,
+                    )
+                }
+            }
+            awaitIdle()
+
+            onNodeWithContentDescription(label = getString(resource = Res.string.content_description_linear_progress_indicator))
+                .assertDoesNotExist()
+        }
+    }
+
+    @Test
+    fun `Should display LinearProgressIndicator if progress is 0`() = runTest {
+        with(compose) {
+            val statusMessage = "some-status-message"
+            val progress = 0f
+
+            setContent {
+                GregoryGreenTheme {
+                    StatusBar(
+                        statusMessage = statusMessage,
+                        progress = progress,
+                    )
+                }
+            }
+            awaitIdle()
+
+            onNodeWithContentDescription(label = getString(resource = Res.string.content_description_linear_progress_indicator)).assertExists()
+        }
+    }
+
+    @Test
+    fun `Should display LinearProgressIndicator if progress is greater than 0`() = runTest {
+        with(compose) {
+            val statusMessage = "some-status-message"
+            val progress = 8.25f
+
+            setContent {
+                GregoryGreenTheme {
+                    StatusBar(
+                        statusMessage = statusMessage,
+                        progress = progress,
+                    )
+                }
+            }
+            awaitIdle()
+
+            onNodeWithContentDescription(label = getString(resource = Res.string.content_description_linear_progress_indicator)).assertExists()
+        }
+    }
+}
